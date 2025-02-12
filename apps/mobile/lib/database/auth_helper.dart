164ecb27a6a04d0a41494349b233c12789118c9b
Omar Syed <omarsyed0504@gmail.com>
2025-02-11 21:25:10 -0500

    Add models for Graph, GraphPoint, Activity, and Notif; implement configuration loading and utility functions

---
+++ apps/mobile/lib/database/auth_helper.dart
@@ -0,0 +1,139 @@
+import 'dart:developer';
+import 'dart:io';
+import 'package:armm_app/database/database.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+
+/// Deletes any user currently in the Firebase Auth buffer.
+Future<void> deleteUserInBuffer() async {
+  if (FirebaseAuth.instance.currentUser != null) {
+    try {
+      await FirebaseAuth.instance.currentUser!.delete();
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        await FirebaseAuth.instance.signOut();
+      } else {
+        log('Error deleting user: $e', stackTrace: StackTrace.current);
+      }
+    }
+    log('User after delete: ${FirebaseAuth.instance.currentUser ?? 'deleted'}');
+  }
+}
+
+/// Handles FirebaseAuthException and displays an error message.
+Future<void> handleFirebaseAuthException(
+    BuildContext context, FirebaseAuthException e, String email) async {
+  String errorMessage = 'Failed to sign up. Please try again.';
+  String? temp = FirebaseAuth.instance.currentUser?.email;
+  switch (e.code) {
+    case 'email-already-in-use':
+      if (FirebaseAuth.instance.currentUser?.email == email) {
+        await deleteUserInBuffer();
+      } 
+      errorMessage =
+          'Email $email is already in use. Please use a different email.';
+      break;
+    case 'invalid-email':
+      errorMessage = '"$email" is not a valid email format. Please try again.';
+      log('Invalid email format.');
+      break;
+    case 'weak-password':
+      errorMessage = 'The password provided is too weak.';
+      log('Weak password.');
+      break;
+    default:
+      log('FirebaseAuthException: $e');
+  }
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: const Text('Error'),
+        content: Text(errorMessage),
+        actions: <Widget>[
+          TextButton(
+            child: const Text('OK'),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
+
+/// Updates Firebase Messaging token.
+Future<void> updateFirebaseMessagingToken(User? user, BuildContext context) async {
+  if (user == null) {
+    return;
+  }
+  String? token;
+  try {
+    token = await FirebaseMessaging.instance.getToken();
+  } catch (e) {
+    log('Error fetching token: $e');
+    token = await FirebaseMessaging.instance.getAPNSToken();
+    log('APNS Token found: $token');
+  }
+  if (token != null) {
+    // Fetch CID using async constructor
+    DatabaseService? db = await DatabaseService.fetchCID(user.uid, context);
+
+    if (db != null) {
+    try {
+      List<dynamic> tokens =
+          (await db.getField('tokens') ?? []);
+
+      if (!tokens.contains(token)) {
+        tokens = [...tokens, token];
+        await db.updateField('tokens', tokens);
+      }
+    } catch (e) {
+      log('login.dart: Error fetching tokens: $e');
+    }
+  }
+  }// async gap widget mounting check
+}
+
+/// Deletes the Firebase Messaging token when the user signs out.
+Future<void> deleteFirebaseMessagingToken(User? user, BuildContext context) async {
+  if (user == null) {
+    log('auth_helper.dart: User is null.'); 
+    return;
+  }
+  // Retrieve the current FCM token
+  String? token;
+  try {
+    token = await FirebaseMessaging.instance.getToken();
+  } catch (e) {
+    log('Error fetching token: $e');
+    token = await FirebaseMessaging.instance.getAPNSToken();
+    log('APNS Token found: $token');
+  }
+
+  if (token != null) {
+    // Fetch the DatabaseService instance for the user
+    DatabaseService? db = await DatabaseService.fetchCID(user.uid, context);
+
+    if (db != null) {
+      try {
+        // Retrieve the current list of tokens from Firestore
+        List<dynamic> tokens = (await db.getField('tokens')) ?? [];
+
+        if (tokens.contains(token)) {
+          // Remove the current token from the list
+          tokens.remove(token);
+
+          // Update the tokens field in Firestore
+          await db.updateField('tokens', tokens);
+          log('Token removed successfully.');
+        }
+      } catch (e) {
+        log('Error deleting token: $e');
+      }
+    }
+  }
+}
---
+++ apps/mobile/lib/database/models/assets_model.dart
@@ -0,0 +1,147 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+/// Represents an individual asset within a fund.
+///
+/// Each asset has a display title, an amount, and an optional first deposit date.
+class Asset {
+  /// The display title of the asset.
+  final String displayTitle;
+
+  /// The amount associated with the asset.
+  final double amount;
+
+  /// The date of the first deposit, if any.
+  final DateTime? firstDepositDate;
+
+  final int index;
+
+  /// Creates an [Asset] instance with the given parameters.
+  Asset({
+    required this.displayTitle,
+    required this.amount,
+    required this.index,
+    this.firstDepositDate,
+  });
+
+  /// Creates an [Asset] instance from a [Map] representation.
+  factory Asset.fromMap(Map<String, dynamic> data) => Asset(
+        displayTitle: data['displayTitle'] as String,
+        amount: (data['amount'] as num).toDouble(),
+        firstDepositDate: (data['firstDepositDate'] as Timestamp?)?.toDate(),
+        index: (data['index'] ?? -1) as int,
+      );
+
+  /// Converts the [Asset] instance into a [Map] representation.
+  Map<String, dynamic> toMap() => {
+        'displayTitle': displayTitle,
+        'amount': amount,
+        'firstDepositDate': firstDepositDate,
+      };
+}
+
+/// Represents a fund containing multiple assets with variable properties.
+///
+/// The `Fund` class now contains a map of asset names to their corresponding [Asset] objects.
+class Fund {
+  /// A map of asset names to their corresponding [Asset] objects.
+  final Map<String, Asset> assets;
+
+  final double total;
+
+  final String name;
+
+  /// Creates a [Fund] instance with the given assets.
+  Fund({
+    required this.assets,
+    required this.total,
+    required this.name,
+  });
+
+  /// Creates a [Fund] instance from a [Map] representation.
+  ///
+  /// This method parses each asset in the map and constructs [Asset] objects.
+  factory Fund.fromMap(Map<String, dynamic> data) {
+    final d = data;
+    final assets = <String, Asset>{};
+    String name = '';
+    double total = 0.0;
+    data.forEach((key, value) {
+      if (key == 'total') {
+        total = (value as num).toDouble();
+      } else if (key == 'fund') {
+        name = value as String;
+      } else {
+        assets[key] = Asset.fromMap(value as Map<String, dynamic>);
+      }
+    });
+    return Fund(assets: assets, total: total, name: name);
+  }
+
+  /// Converts the [Fund] instance into a [Map] representation.
+  Map<String, dynamic> toMap() => assets.map(
+        (key, value) => MapEntry(key, value.toMap()),
+      );
+
+  /// Creates an empty [Fund] instance with no assets.
+  Fund.empty() : assets = {}, total = 0.0, name = '';
+}
+
+/// Represents the assets of a client, including various funds and totals.
+///
+/// The `Assets` class aggregates all the financial assets of a client, including individual funds
+/// and overall totals for year-to-date (YTD) and total assets.
+class Assets {
+  /// A map of fund names to their corresponding [Fund] objects.
+  final Map<String, Fund> funds;
+
+  /// The total year-to-date amount for the client, including their connected users.
+  final double? totalYTD;
+
+  /// The year-to-date amount for the client.
+  final double? ytd;
+
+  /// The total assets amount for the client.
+  final double? totalAssets;
+
+  /// Creates an [Assets] instance with the given parameters.
+  ///
+  /// The [funds], [ytd], and [totalAssets] parameters are required.
+  Assets({
+    required this.funds,
+    required this.ytd,
+    required this.totalAssets,
+    this.totalYTD,
+  });
+
+  /// Creates an [Assets] instance from a [Map] representation.
+  ///
+  /// [fundsData] is a map of fund names to their data maps.
+  /// [general] contains general information such as totals.
+  factory Assets.fromMap(
+    Map<String, Fund> funds,
+    Map<String, dynamic> general,
+  ) => Assets(
+      totalYTD: (general['totalYTD'] as num?)?.toDouble(),
+      ytd: (general['ytd'] as num?)?.toDouble(),
+      totalAssets:
+          (general['totalAssets'] ?? general['total'] as num?)?.toDouble(),
+      funds: funds,
+    );
+
+  /// Creates an empty [Assets] instance with default values.
+  Assets.empty()
+      : funds = {},
+        totalYTD = 0.0,
+        ytd = 0.0,
+        totalAssets = 0.0;
+
+  /// Converts the [Assets] instance into a [Map] representation.
+  Map<String, dynamic> toMap() => {
+        'funds': funds.map(
+          (key, value) => MapEntry(key, value.toMap()),
+        ),
+        'totalYTD': totalYTD,
+        'ytd': ytd,
+        'totalAssets': totalAssets,
+      };
+}
