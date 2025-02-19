9c4cce8ce4a075cd35b07d5897ce3b965884ce72
Omar Syed <omarsyed0504@gmail.com>
2025-02-19 12:32:35 -0500

    Updated Client Model and Completed Profiles Page

--- apps/mobile/lib/screens/profile/pages/my_profiles_page.dart
+++ apps/mobile/lib/screens/profile/pages/my_profiles_page.dart
@@ -1,18 +1,223 @@
-import 'package:flutter/material.dart';
-import 'package:armm_app/utils/app_bar.dart';
+// ignore_for_file: deprecated_member_use, use_build_context_synchronously, duplicate_ignore, prefer_expression_function_bodies, unused_catch_clause, empty_catches
 
-class MyProfilesPage extends StatelessWidget {
+import 'package:armm_app/database/models/client_model.dart';
+import 'package:armm_app/screens/profile/components/name_cid.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class MyProfilesPage extends StatefulWidget {
+  const MyProfilesPage({Key? key}) : super(key: key);
+  @override
+  // ignore: library_private_types_in_public_api
+  _MyProfilesPageState createState() => _MyProfilesPageState();
+}
+
+class _MyProfilesPageState extends State<MyProfilesPage> {
+  Client? client;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    client = Provider.of<Client?>(context);
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (client == null) {
+      return const CircularProgressIndicator();
+    }
+
     return Scaffold(
-      appBar: CustomAppBar(
-        title: 'My Profiles',
-        implyLeading: true,
-        showNotificationButton: false,
-      ),
-      body: Center(
-        child: Text('My Profiles Page'),
+      body: Stack(
+        children: [
+          CustomScrollView(
+            slivers: <Widget>[
+              _buildAppBar(context),
+              SliverPadding(
+                padding: const EdgeInsets.all(0.0),
+                sliver: SliverList(
+                  delegate: SliverChildListDelegate(
+                    [
+                      _profilesForUser(),
+                      if (client!.connectedUsers != null &&
+                          client!.connectedUsers!.isNotEmpty)
+                        _profilesForConnectedUser()
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
+
+// This is the Profiless section
+  Container _profilesForUser() => Container(
+    padding: const EdgeInsets.fromLTRB(20, 20, 20, 40),
+    child: Column(
+      children: [
+
+        clientCard(client!),
+
+      ],
+    ),
+  
+  );
+
+// This is the Profiless section
+  Container _profilesForConnectedUser() {
+    if (client!.connectedUsers == null || client!.connectedUsers!.isEmpty) {
+      return Container();
+    }
+    return Container(
+      padding: const EdgeInsets.fromLTRB(20, 10, 20, 40),
+      child: Column(
+        children: [
+          const Row(
+            children: [
+              Text(
+                'Connected Users',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  
+                ),
+              ),
+            ],
+          ),
+          ListView.builder(
+            padding: const EdgeInsets.only(top: 20),
+            itemCount: client!.connectedUsers!.length,
+            itemBuilder: (context, index) {
+              return 
+              Column(
+                children: [
+                  clientCard(client!.connectedUsers![index]!),
+                  const SizedBox(height: 20),
+                ],
+              );
+            },
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget clientCard(Client c) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.transparent,
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: Colors.black, width: 1),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // Client's Name
+            Text(
+              '${c.firstName} ${c.lastName}',
+              style: const TextStyle(
+                fontSize: 18,
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 5),
+            // Client ID
+            Text(
+              'Client ID: ${c.cid}',
+              style: const TextStyle(
+                fontSize: 14,
+                color: Colors.blue,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            const SizedBox(height: 15),
+            // First Deposit Date
+            Text(
+              'First Deposit Date: ${c.firstDepositDate.toString()}',
+              style: const TextStyle(
+                fontSize: 14,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 10),
+            // Communication Email
+            Text(
+              'Communication Email: ${c.initEmail}',
+              style: const TextStyle(
+                fontSize: 14,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 10),
+            // Phone Number
+            Text(
+              'Phone Number: ${c.phoneNumber}',
+              style: const TextStyle(
+                fontSize: 14,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 10),
+            // Address
+            Text(
+              'Address: ${c.address}',
+              style: const TextStyle(
+                fontSize: 14,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(height: 10),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // This is the app bar
+  SliverAppBar _buildAppBar(context) => SliverAppBar(
+        backgroundColor: const Color.fromARGB(255, 30, 41, 59),
+        automaticallyImplyLeading: false,
+        toolbarHeight: 80,
+        expandedHeight: 0,
+        snap: false,
+        floating: true,
+        pinned: true,
+        leading: IconButton(
+          icon:
+              const Icon(Icons.arrow_back_ios_new_rounded, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        flexibleSpace: const SafeArea(
+          child: Padding(
+            padding: EdgeInsets.only(left: 60.0, right: 20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Profiles',
+                  style: TextStyle(
+                    fontSize: 27,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
 }
