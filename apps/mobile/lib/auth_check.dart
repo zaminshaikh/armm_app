6091d6a45c460e84e9b09bbc198ff6848e2785d7
Zamin Shaikh <zaminusa@gmail.com>
2025-02-27 16:18:34 -0500

    Add AuthCheck file to manage user authentication and verification flow, triggered on Firebase Auth stream

---
+++ apps/mobile/lib/auth_check.dart
@@ -0,0 +1,84 @@
+import 'package:armm_app/auth/onboarding/onboarding_page.dart';
+import 'package:armm_app/database/database.dart';
+import 'package:armm_app/screens/profile/profile.dart';
+import 'package:armm_app/signup_data.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthCheck extends StatefulWidget {
+  const AuthCheck({Key? key}) : super(key: key);
+
+  @override
+  _AuthCheckState createState() => _AuthCheckState();
+}
+
+class _AuthCheckState extends State<AuthCheck> {
+  late Future<bool> _isAuthenticatedAndVerifiedFuture;
+
+  /// Check if the user is authenticated, email verified, and linked
+  Future<bool> isAuthenticatedAndVerified() async {
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      return false;
+    }
+
+    await user.reload(); 
+
+    if (!user.emailVerified) {
+      return false;
+    }
+
+    String uid = user.uid;
+
+    DatabaseService db = DatabaseService(uid);
+
+    bool isLinked = await db.isUIDLinked(uid);
+
+    return isLinked;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _isAuthenticatedAndVerifiedFuture = isAuthenticatedAndVerified();
+  }
+
+
+  @override
+  Widget build(BuildContext context) => StreamBuilder<User?>(
+    stream: FirebaseAuth.instance.userChanges(),
+    builder: (BuildContext context,AsyncSnapshot<User?> snapshot) {
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return const Center(child: CircularProgressIndicator());
+      } else if (snapshot.hasError) {
+        return Center(child: Text('Error: ${snapshot.error}'));
+      } else if (snapshot.hasData) {
+        final user = snapshot.data;
+        if (user != null) {
+        // User is signed in
+        return FutureBuilder(
+          future: _isAuthenticatedAndVerifiedFuture,
+          builder:  (context, authSnapshot) {
+            if (authSnapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (authSnapshot.hasError) {
+              return Center(child: Text('Error: ${authSnapshot.error}'));
+            } else if (authSnapshot.hasData && authSnapshot.data == true) {
+              // User is authenticated, email verified, and linked
+              return const ProfilePage();
+            } else {
+              // User is not authenticated, email not verified, or not linked
+              return OnboardingPage(signUpData: SignUpData());
+            }
+          });
+        } else {
+          // User is not signed in
+          return OnboardingPage(signUpData: SignUpData());
+        }
+      } else {
+        return OnboardingPage(signUpData: SignUpData());
+      }
+    },
+  );
+  
+}
