c4a00f8c994c16f41a0e4d0d11ce02261b3a6a45
Omar Syed <omarsyed0504@gmail.com>
2025-03-12 17:30:12 -0400

    Add notification icon and update CustomAppBar for improved UI

--- apps/mobile/lib/utils/app_bar.dart
+++ apps/mobile/lib/utils/app_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   final String title;
@@ -28,7 +29,7 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
               gradient: LinearGradient(
                 colors: [
                   Color(0xFF2B41B8),
-                  Color.fromARGB(255, 169, 175, 206),
+                  Color.fromARGB(255, 55, 82, 233),
                 ],
                 begin: Alignment.topLeft,
                 end: Alignment.bottomRight,
@@ -52,22 +53,24 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
           toolbarHeight: 100,
           backgroundColor: Colors.transparent, // Allows the gradient to show
           centerTitle: true,
-          title: Text(
-            title,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 25,
-              fontWeight: FontWeight.w700,
+            title: Text(
+              title,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 25,
+                fontWeight: FontWeight.w600,
+                fontFamily: 'Inter',
+              ),
             ),
-          ),
           actions: showNotificationButton
               ? [
                   Padding(
-                    padding: const EdgeInsets.only(right: 20.0),
+                    padding: const EdgeInsets.only(right: 10.0),
                     child: IconButton(
-                      icon: const Icon(
-                        Icons.notifications_none,
-                        size: 30,
+                      icon: SvgPicture.asset(
+                        'assets/icons/notification.svg',
+                        width: 30,
+                        height: 30,
                         color: Colors.white,
                       ),
                       onPressed: onNotificationTap ?? () {},
