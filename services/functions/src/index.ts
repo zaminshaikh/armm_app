2297ea721b30a0dca93fdeee049e71a73f396e6d
Zamin Shaikh <zaminusa@gmail.com>
2025-05-08 12:47:00 -0400

    Remove unused addStatement function and add handleStorageDocumentUpload trigger for Firestore storage events

--- services/functions/src/index.ts
+++ services/functions/src/index.ts
@@ -14,6 +14,7 @@ import { handleActivity, onActivityWrite } from "./triggers/activityTriggers";
 import { onAssetUpdate } from "./triggers/assetTriggers";
 import { onConnectedUsersChange } from "./triggers/connectedUsersTriggers";
 import { githubToNotion } from "./triggers/githubToNotion";
+import { handleStorageDocumentUpload } from "./triggers/documentTriggers"; // Added import
 
 // ======= SCHEDULED =======
 import { scheduledYTDReset } from "./scheduled/scheduledReset";
@@ -26,7 +27,7 @@ import { unlinkUser } from "./callable/unlinkUser";
 import { calculateTotalYTD, calculateYTD } from "./callable/ytd";
 
 // Expose Firestore triggers
-export { handleActivity, onActivityWrite, onAssetUpdate, onConnectedUsersChange, githubToNotion};
+export { handleActivity, onActivityWrite, onAssetUpdate, onConnectedUsersChange, githubToNotion, handleStorageDocumentUpload }; // Added export
 
 // Expose scheduled tasks
 export { scheduledYTDReset, processScheduledActivities };
