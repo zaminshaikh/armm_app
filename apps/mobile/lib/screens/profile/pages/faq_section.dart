a2c9e82f89b1fed4c9ae54300fd93b5e2cffbf2d
Zamin Shaikh <zaminusa@gmail.com>
2025-06-25 20:47:41 -0700

    Make FAQ visible.

--- apps/mobile/lib/screens/profile/pages/faq_section.dart
+++ apps/mobile/lib/screens/profile/pages/faq_section.dart
@@ -1,3 +1,4 @@
+import 'package:armm_app/utils/resources.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
@@ -20,52 +21,78 @@ class _FAQSectionState extends State<FAQSection> {
     FAQItem(
       question: "What should I do if I forget my password?",
       answer: [
-        Text("On the login screen, tap “Forgot Password” and follow the steps to securely reset your credentials via email or SMS verification."),
+        Text(
+          "On the login screen, tap “Forgot Password” and follow the steps to securely reset your credentials via email or SMS verification.",
+          style: GoogleFonts.inter(color: Colors.black),
+        ),
       ],
     ),
     FAQItem(
       question: "Is the ARMM app available on both iOS and Android?",
       answer: [
-        Text("The ARMM Group app is available on both Android and iOS for download on the Apple App Store and Google Play Store."),
+        Text(
+          "The ARMM Group app is available on both Android and iOS for download on the Apple App Store and Google Play Store.",
+          style: GoogleFonts.inter(color: Colors.black),
+        ),
       ],
     ),
     FAQItem(
       question: "How do I enable or disable push notifications?",
       answer: [
-        Text("You can manage notification preferences under \"Profile Settings\" within the app to stay informed about investment updates, document uploads, and account activity."),
+        Text(
+          "You can manage notification preferences under \"Profile Settings\" within the app to stay informed about investment updates, document uploads, and account activity.",
+          style: GoogleFonts.inter(color: Colors.black),
+        ),
       ],
     ),
     FAQItem(
       question: "Is there a minimum account balance required to use the ARMM app?",
       answer: [
-        Text("There is no minimum balance required to access the app; however, investment offerings may have their own minimum contribution requirements."),
+        Text(
+          "There is no minimum balance required to access the app; however, investment offerings may have their own minimum contribution requirements.",
+          style: GoogleFonts.inter(color: Colors.black),
+        ),
       ],
     ),
     FAQItem(
       question: "How often are transactions updated on the ARMM Group App?",
       answer: [
-        Text("Transactions on the ARMM Group App are updated regularly, with most activity reflected in real-time or promptly upon confirmation. This allows you to monitor your investment transactions accurately and without delay."),
+        Text(
+          "Transactions on the ARMM Group App are updated regularly, with most activity reflected in real-time or promptly upon confirmation. This allows you to monitor your investment transactions accurately and without delay.",
+          style: GoogleFonts.inter(color: Colors.black),
+        ),
       ],
     ),
     FAQItem(
       question: "How do I request an account statement?",
       answer: [
-        Text("To request an account statement, simply email your request—along with the specific months or date range you need—to one of the following:"),
+        Text(
+          "To request an account statement, simply email your request—along with the specific months or date range you need—to one of the following:",
+          style: GoogleFonts.inter(color: Colors.black),
+        ),
         GestureDetector(
           onTap: () => launchUrl(Uri.parse('mailto:info@armmgroup.com')),
-          child: Text("info@armmgroup.com" ),
+          child: Text("info@armmgroup.com",
+              style: GoogleFonts.inter(color: AppColors.primary,)),
         ),
         GestureDetector(
           onTap: () => launchUrl(Uri.parse('mailto:mohammed@armmgroup.com')),
-          child: Text("mohammed@armmgroup.com" ),
+          child: Text("mohammed@armmgroup.com",
+              style: GoogleFonts.inter(color: AppColors.primary,)),
         ),
-        Text("Your request will typically be processed within 24 hours. For urgent needs, feel free to contact your ARMM advisor directly."),
+        Text(
+          "Your request will typically be processed within 24 hours. For urgent needs, feel free to contact your ARMM advisor directly.",
+          style: GoogleFonts.inter(color: Colors.black),
+        ),
       ],
     ),
     FAQItem(
       question: "Can I add to my existing investment?",
       answer: [
-        Text("Yes. You can make additional investments into your current portfolio. Contact your ARMM Group representative for guidance on the process."),
+        Text(
+          "Yes. You can make additional investments into your current portfolio. Contact your ARMM Group representative for guidance on the process.",
+          style: GoogleFonts.inter(color: Colors.black),
+        ),
       ],
     ),
   ];
@@ -101,8 +128,8 @@ class _FAQSectionState extends State<FAQSection> {
                 highlightColor: Colors.transparent,
               ),
               child: ExpansionTile(
-                tilePadding: const EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-                childrenPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+                tilePadding: const EdgeInsets.symmetric(horizontal: 12, vertical: 0),
+                childrenPadding: const EdgeInsets.symmetric(horizontal: 12, vertical: 0),
                 title: Text(
                   item.question,
                   style: GoogleFonts.inter(fontWeight: FontWeight.w600),
@@ -119,7 +146,7 @@ class _FAQSectionState extends State<FAQSection> {
                 children: [
                   Container(
                     width: double.infinity,
-                    padding: const EdgeInsets.all(8),
+                    padding: const EdgeInsets.symmetric(vertical: 8),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: item.answer,
