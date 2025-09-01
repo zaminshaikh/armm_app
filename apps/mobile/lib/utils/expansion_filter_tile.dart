b206bdb3de37f03b6999466ed9c6b88087eaed4b
Zamin Shaikh <zaminusa@gmail.com>
2025-09-01 17:31:52 -0400

    Fix toTitleCase function to handle empty words correctly

--- apps/mobile/lib/utils/expansion_filter_tile.dart
+++ apps/mobile/lib/utils/expansion_filter_tile.dart
@@ -7,7 +7,7 @@ String toTitleCase(String text) {
   if (text.isEmpty) return text;
   return text
       .split(' ')
-      .map((word) => word[0].toUpperCase() + word.substring(1))
+      .map((word) => word.isEmpty ? word : word[0].toUpperCase() + word.substring(1))
       .join(' ');
 }
 
